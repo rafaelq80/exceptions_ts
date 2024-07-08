58f29fe710d611b527910dc73a69b6a1dc84a78d
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-07-08 20:31:24 -0300

    Código Base

---
+++ Conversor.ts
@@ -0,0 +1,8 @@
+﻿
+const resultado = converte('abc');
+
+console.log(resultado);
+
+export function converte(conteudo: any): string {
+    return conteudo.toUpperCase();
+}
---
+++ ValidaIdade.ts
@@ -0,0 +1,9 @@
+﻿import readline = require("readline-sync");
+
+        let idade: number = readline.questionInt("Digite a idade: ")
+        validarIdade(idade)
+
+function validarIdade(idade: number): void {
+    if (idade > 18)
+        console.log("A pessoa é maior de idade!")
+}
